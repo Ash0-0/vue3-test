ce47784d5abc121f557b6ba669deae64d772ec54
ash <hongyu.ash.chen@gmail.com>
2024-12-02 23:23:14 +0800

    test

--- scripts/deploy/Jenkinsfile.groovy
+++ scripts/deploy/Jenkinsfile.groovy
@@ -29,7 +29,7 @@ pipeline {
                     // 使用 pnpm 生成新版本号并提交更新
                     sh '''
                     corepack enable
-                    corepack prepare pnpm@latest-10 --activate
+                    corepack prepare pnpm@stable --activate
                     pnpm version patch --no-git-tag-version // 递增 PATCH 版本
                     NEW_VERSION=$(node -p "require('./package.json').version") // 获取新版本号
                     echo "NEW_VERSION=${NEW_VERSION}" > version.env // 将版本号保存到文件
